c62b21b51900dc6f1575d210905add4f3b926f9c
mehdi-alouane <pro4medo@gmail.com>
2019-06-29 07:44:28 +0100

    refactor: mount user route on api router

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,5 +1,8 @@
 const v1Router = require('express').Router()
+const userRouter = require('./user')
 
 v1Router.get('/echo', (req, res) => res.send('echo!'))
+
+v1Router.use('/user', userRouter)
 
 module.exports = v1Router
