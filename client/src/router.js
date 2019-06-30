fb3962e7f65c82a41c1ef1e6bc543ee21b80bfed
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 22:04:26 +0100

    refactor: update router routes

--- client/src/router.js
+++ client/src/router.js
@@ -1,25 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+
+const homeView = () => import('./views/Home.vue')
+const registerView = () => import('./views/Register.vue')
+const loginView = () => import('./views/Login.vue')
+
+const routes = [
+  { path: '/', name: 'home', component: homeView },
+  { path: '/register', name: 'register', component: registerView },
+  { path: '/login', name: 'login', component: loginView }
+]
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
+  routes
 })
