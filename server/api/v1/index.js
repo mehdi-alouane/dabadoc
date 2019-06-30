7c8731d81584e1d86b92f272a8c191cecd0f93d2
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 20:28:18 +0100

    refactor: add question and asnwers routes

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -1,8 +1,12 @@
 const v1Router = require('express').Router()
 const userRouter = require('./user')
+const questionRouter = require('./question')
+const answerRouter = require('./answer')
 
 v1Router.get('/echo', (req, res) => res.send('echo!'))
 
 v1Router.use('/user', userRouter)
+v1Router.use('/question', questionRouter)
+v1Router.use('/answer', answerRouter)
 
 module.exports = v1Router
