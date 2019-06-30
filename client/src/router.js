dab3b9fb82ce6460aa2a303fb0cc821b5792b9d5
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 00:32:14 +0100

    refactor: add question route to vue router

--- client/src/router.js
+++ client/src/router.js
@@ -4,11 +4,13 @@ import Router from 'vue-router'
 const homeView = () => import('./views/Home.vue')
 const registerView = () => import('./views/Register.vue')
 const loginView = () => import('./views/Login.vue')
+const questionView = () => import('./views/Question.vue')
 
 const routes = [
   { path: '/', name: 'home', component: homeView },
   { path: '/register', name: 'register', component: registerView },
-  { path: '/login', name: 'login', component: loginView }
+  { path: '/login', name: 'login', component: loginView },
+  { path: '/question', name: 'question', component: questionView }
 ]
 
 Vue.use(Router)
