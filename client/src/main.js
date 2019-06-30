a7163610eb691a164b8134a633bb7075ea2267ab
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 21:14:34 +0100

    refactor: add axiso and geolocation to main.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueGeolocation from 'vue-browser-geolocation'
+import axios from '@/services/axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueGeolocation)
+Vue.use(VueAxios, axios)
 
 Vue.config.productionTip = false
 
