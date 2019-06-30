7467af977ad54b114bd8188b281a1025970825d3
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 00:33:01 +0100

    feat: create add question view

---
+++ client/src/components/addQuestion.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="box">
+    <div class="field">
+      <label class="label">Question Title:</label>
+      <div class="control">
+        <input
+          v-model="question.title"
+          type="text"
+          class="input"
+          placeholder="Add your question title here"
+        >
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Question:</label>
+      <div class="control">
+        <textarea
+          v-model="question.content"
+          class="textarea"
+          placeholder="Add a your question here"
+        />
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Location:</label>
+      <div class="control">
+        <input
+          v-model="location"
+          type="text"
+          class="input"
+        >
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddQuestion',
+  props: {
+    location: {
+      type: String,
+      default: ''
+    }
+  },
+  data: () => ({
+    question: {
+      title: null,
+      content: null
+    }
+  })
+}
+</script>
---
+++ client/src/views/Question.vue
@@ -0,0 +1,37 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="columns is-centered">
+        <div class="column is-6">
+          <h1 class="title is-3">
+            Add A Question
+          </h1>
+          <add-question :location="coordinates" />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import addQuestion from '@/components/addQuestion'
+
+export default {
+  components: { addQuestion },
+  data: () => ({
+    coordinates: null
+  }),
+
+  created () {
+    this.getCurrentLocation()
+  },
+
+  methods: {
+    async getCurrentLocation () {
+      const coordinates = await this.$getLocation()
+      this.coordinates = [coordinates.lat, coordinates.lng].join(',')
+    }
+  }
+}
+</script>
+
