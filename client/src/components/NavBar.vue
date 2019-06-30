f5ea1cc0f4ffc4cc685fe9b9f523eb25ca3a369b
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 00:32:42 +0100

    refactor: update navbar links

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="navbar is-info has-shadow">
-    <div class="navbar-menu navbar-end">
-      <router-link
-        to=""
-        class="navbar-item"
-      >
-        Favourite
-      </router-link>
+    <div class="container">
+      <div class="navbar-menu">
+
+        <div class="navbar-start">
+          <router-link
+            :to="{ name: 'question' }"
+            class="navbar-item">
+            <span class="icon">
+              <i class="fa fa-plus"></i>
+            </span>
+            <span>Add a question</span>
+          </router-link>
+        </div>
+
+        <div class="navbar-end">
+          <router-link
+            to=""
+            class="navbar-item"
+          >
+          <span class="icon">
+            <i class="far fa-bookmark"></i>
+          </span>
+          <span>Favourite</span>
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
