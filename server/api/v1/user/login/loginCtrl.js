5a6edc0ce8439665e50124bd40bb73df8a4cc388
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 20:29:17 +0100

    feat: create user login controller

---
+++ server/api/v1/user/login/loginCtrl.js
@@ -0,0 +1,42 @@
+const { User } = require('../userModel')
+const bcrypt = require('bcryptjs')
+const { sign } = require('jsonwebtoken')
+const { jwt } = require('../../../../config/config.json')
+
+module.exports = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(401).json({
+        auth: false,
+        msg: 'Something wrong please try again!'
+      })
+    }
+
+    const isPasswordValid = bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+      return res.status(401).json({
+        auth: false,
+        msg: 'Something wrong please try again!'
+      })
+    }
+
+    const token = sign({
+      id: user.id
+    }, jwt.secret)
+
+    return res.status(200).json({
+      auth: true,
+      token: token
+    })
+  } catch (err) {
+    console.log(`Login Auth: ${err.message}`)
+    return res.status(500).json({
+      auth: false,
+      msg: err.message
+    })
+  }
+}
