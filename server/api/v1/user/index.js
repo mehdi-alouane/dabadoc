9c542bbbe1662ec53203db933b9a255c1b672aaf
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 20:28:58 +0100

    refactor: add login route to user router

--- server/api/v1/user/index.js
+++ server/api/v1/user/index.js
@@ -1,8 +1,11 @@
 const userRouter = require('express').Router()
 
 const registerCtrl = require('./register/registerCtrl')
+const loginCtrl = require('./login/loginCtrl')
 
 userRouter.route('/register')
   .post(registerCtrl)
+userRouter.route('/login')
+  .post(loginCtrl)
 
 module.exports = userRouter
