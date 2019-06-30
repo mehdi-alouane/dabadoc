ac23aa89baeeb3be34e3bd8d11be1d0d55cd83d6
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 22:04:08 +0100

    refactor: add styles to index instead of App

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <div id="app">
-    <router-view/>
+    <navbar />
+    <router-view />
   </div>
 </template>
 
-<style lang="sass" src="bulma">
-</style>
+<script>
+import navbar from '@/components/NavBar'
 
-<style lang="scss" src="../node_modules/font-awesome/scss/font-awesome.scss">
-</style>
+export default {
+  components: { navbar }
+}
+</script>
