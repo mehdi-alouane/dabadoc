31d8982979d27b095d84c781c2413cdce2a6ab34
mehdi-alouane <pro4medo@gmail.com>
2019-06-30 22:04:53 +0100

    feat: add user auth to store

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,43 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from '@/services/axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    token: null,
+    user: null,
+    isLoggedIn: false
   },
   mutations: {
-
+    setToken (state, token) {
+      state.token = token
+      if (token) {
+        state.isLoggedIn = true
+      }
+    },
+    setUser (state, user) {
+      state.user = user
+    }
   },
   actions: {
+    async setToken ({ commit }, { email, password }) {
+      const { data } = await axios.post('/user/login', {
+        email,
+        password
+      })
+      commit('setToken', data.token)
+    },
+    async setUser ({ commit }) {
+      const token = this.state.token
+      const auth = await axios.get('/user/me', {
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
 
+      commit('setUser', auth.data)
+    }
   }
 })
