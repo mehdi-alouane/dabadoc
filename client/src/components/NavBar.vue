c9ba1facbbb2db04ca03291fb6777886abcb01a0
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 03:18:50 +0100

    refactor: add icons to navbar links

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -2,13 +2,13 @@
   <div class="navbar is-info has-shadow">
     <div class="container">
       <div class="navbar-menu">
-
         <div class="navbar-start">
           <router-link
             :to="{ name: 'question' }"
-            class="navbar-item">
+            class="navbar-item"
+          >
             <span class="icon">
-              <i class="fa fa-plus"></i>
+              <i class="fa fa-plus" />
             </span>
             <span>Add a question</span>
           </router-link>
@@ -19,10 +19,10 @@
             to=""
             class="navbar-item"
           >
-          <span class="icon">
-            <i class="far fa-bookmark"></i>
-          </span>
-          <span>Favourite</span>
+            <span class="icon">
+              <i class="far fa-bookmark" />
+            </span>
+            <span>Favourite</span>
           </router-link>
         </div>
       </div>
