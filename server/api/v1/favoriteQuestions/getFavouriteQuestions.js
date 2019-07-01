36d10ad6099c07427eccc99e4e96b1e3be492283
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 09:20:10 +0100

    feat: create favourites controller

---
+++ server/api/v1/favoriteQuestions/getFavouriteQuestions.js
@@ -0,0 +1,28 @@
+const { User } = require('../user/userModel')
+
+module.exports = async (req, res) => {
+  try {
+    const { userID } = req.params
+    const favouriteQuestions = await User
+      .find({ _id: userID })
+      .populate({
+        path: 'questions',
+        model: 'Question',
+        select: '_id title content location'
+      })
+      .select('-_id questions')
+
+    if (!favouriteQuestions) {
+      res.status(400).json({
+        msg: 'Enter a valide question'
+      })
+    }
+
+    return res.status(200).json(favouriteQuestions)
+  } catch (err) {
+    console.log(`AddToFavouritesCtrl: ${err.message}`)
+    return res.status(500).json({
+      msg: err.message
+    })
+  }
+}
