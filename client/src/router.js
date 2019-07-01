746c2c17d4edc1a89d04c5828a508904c408f042
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 11:17:54 +0100

    feat: add meta tag to home route

--- client/src/router.js
+++ client/src/router.js
@@ -1,24 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import store from './store'
 
 const homeView = () => import('./views/Home.vue')
 const registerView = () => import('./views/Register.vue')
 const loginView = () => import('./views/Login.vue')
 const questionView = () => import('./views/Question.vue')
 const answerView = () => import('./views/Answer.vue')
+const favouritesView = () => import('./views/Favourites.vue')
 
 const routes = [
-  { path: '/', name: 'home', component: homeView },
+  { path: '/', name: 'home', component: homeView, meta: { requiresAuth: true } },
   { path: '/register', name: 'register', component: registerView },
   { path: '/login', name: 'login', component: loginView },
   { path: '/question', name: 'question', component: questionView },
-  { path: '/answer/:questionID', name: 'answer', component: answerView }
+  { path: '/answer/:questionID', name: 'answer', component: answerView },
+  { path: '/favourites', name: 'favourite', component: favouritesView }
 ]
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    if (!store.state.isLoggedIn) {
+      next({ name: 'login' })
+    } else {
+      store.dispatch('setUser')
+    }
+    next()
+  }
+  next()
+})
+
+export default router
