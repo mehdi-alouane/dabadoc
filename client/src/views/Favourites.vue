21593575c18173d29dcc6271589dd0479753a536
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 11:17:14 +0100

    feat: create favourite questions view

---
+++ client/src/views/Favourites.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <div class="box">
+        <p>{{ favourites }}</p>
+      </div>
+
+      <h1 class="title">
+        Favourites questions:
+      </h1>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    favourites: null
+  }),
+  created () {
+    this.getFavouriteQuestions()
+  },
+  methods: {
+    async getFavouriteQuestions () {
+      const userID = this.$store.state.user.id
+      const favourites = await this.axios.get(`/favourite/list/${userID}`)
+      this.favourites = favourites.data
+      console.log(this.favourites)
+    }
+  }
+}
+</script>
