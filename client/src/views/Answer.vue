b0ae6dcb5163595812aee2be5e350f90d8f5af32
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 03:44:38 +0100

    refactor: add reply to question logic

--- client/src/views/Answer.vue
+++ client/src/views/Answer.vue
@@ -15,12 +15,34 @@
 
       <div class="field" />
       <div class="box">
+        <div
+          v-if="hasErr"
+          class="notification is-warning"
+        >
+          You can't submit blank asnwer, please add a question!
+        </div>
+
         <div class="field">
           <div class="control">
             <textarea
+              v-model="content"
               class="textarea"
               placeholder="Add a answer"
             />
+          </div>
+        </div>
+
+        <div class="field">
+          <div class="control">
+            <button
+              class="button is-primary"
+              @click.prevent="replyToQuestion"
+            >
+              <span class="icon">
+                <i class="fa fa-reply" />
+              </span>
+              <span>Repy</span>
+            </button>
           </div>
         </div>
       </div>
@@ -31,7 +53,10 @@
 <script>
 export default {
   data: () => ({
-    answers: ''
+    answers: '',
+    content: null,
+    hasErr: false,
+    errMsg: null
   }),
   created () {
     this.getQuestionAnswers()
@@ -41,6 +66,26 @@ export default {
       const questionID = this.$route.params.questionID
       const answers = await this.axios.get(`/answer/list/${questionID}`)
       this.answers = answers.data.answers
+    },
+
+    async replyToQuestion () {
+      try {
+        const questionID = this.$route.params.questionID
+        if (!this.content || !questionID) {
+          this.hasErr = true
+        } else {
+          const addAnAnswer = await this.axios.post('/answer/create', {
+            questionID,
+            content: this.content
+          })
+          this.content = ''
+          console.log(addAnAnswer)
+        }
+      } catch (err) {
+        this.hasErr = true
+        this.errMsg = err.message
+        console.log(err.message)
+      }
     }
   }
 }
