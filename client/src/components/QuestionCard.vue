27d089b21ca8f532ef516c41b85126105f0e299c
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 03:19:00 +0100

    feat: create question card

---
+++ client/src/components/QuestionCard.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="card">
+    <header class="card-header">
+      <p class="card-header-title">
+        {{ question.title }}
+      </p>
+    </header>
+
+    <div class="card-content">
+      <div class="content">
+        {{ question.title }}
+      </div>
+    </div>
+
+    <footer class="card-footer">
+      <router-link
+        :to="{ name: 'answer', params: { questionID: question._id } }"
+        class="card-footer-item"
+      >
+        <span class="icon">
+          <i class="fa fa-reply" />
+        </span>
+        <span>Answer</span>
+      </router-link>
+
+      <router-link
+        to=""
+        class="card-footer-item"
+      >
+        <span class="icon">
+          <i class="fa fa-thumbs-up" />
+        </span>
+        <span>Like</span>
+      </router-link>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'QuestionCard',
+  props: {
+    question: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
