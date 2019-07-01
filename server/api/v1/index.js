36fa0e04385aa7482b5b4fbbd27632c3105887f8
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 09:08:33 +0100

    refactor: add favourite route to api router

--- server/api/v1/index.js
+++ server/api/v1/index.js
@@ -2,11 +2,13 @@ const v1Router = require('express').Router()
 const userRouter = require('./user')
 const questionRouter = require('./question')
 const answerRouter = require('./answer')
+const favoriteQuestionsRouter = require('./favoriteQuestions')
 
 v1Router.get('/echo', (req, res) => res.send('echo!'))
 
 v1Router.use('/user', userRouter)
 v1Router.use('/question', questionRouter)
 v1Router.use('/answer', answerRouter)
+v1Router.use('/favourite', favoriteQuestionsRouter)
 
 module.exports = v1Router
