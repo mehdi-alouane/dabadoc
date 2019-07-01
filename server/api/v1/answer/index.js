82a4de0dd20419e7ab2802dcb091dd2b49b15422
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 03:42:10 +0100

    refactor: update answer router routes

--- server/api/v1/answer/index.js
+++ server/api/v1/answer/index.js
@@ -1,8 +1,11 @@
 const answerRouter = require('express').Router()
 
 const postAnswerCtrl = require('./PostAnswer')
+const getAnswerCtrl = require('./GetAnswers')
 
 answerRouter.route('/create')
   .post(postAnswerCtrl)
+answerRouter.route('/list/:questionID')
+  .get(getAnswerCtrl)
 
 module.exports = answerRouter
