4fbb74af7b19690118d05b0e727d6e3e4c0e0bf7
mehdi-alouane <pro4medo@gmail.com>
2019-07-01 03:21:21 +0100

    feat: home now can show questions by distance

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,3 +1,50 @@
 <template>
-  <h1>Home</h1>
+  <section class="section">
+    <div class="container">
+      <h1 class="title">
+        Questions by distance:
+      </h1>
+      <div class="columns is-multiline is-mobile">
+        <div
+          v-for="question in questions"
+          :key="question.id"
+          class="column is-4"
+        >
+          <question-card :question="question" />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
+
+<script>
+import questionCard from '@/components/QuestionCard'
+
+export default {
+  name: 'Home',
+  components: {
+    questionCard
+  },
+  data: () => ({
+    coordinates: null,
+    questions: []
+  }),
+
+  async created () {
+    const coordinates = await this.$getLocation()
+    this.coordinates = [coordinates.lat, coordinates.lng].join(',')
+    this.getQuestionsByDistance()
+  },
+
+  methods: {
+    async getQuestionsByDistance () {
+      try {
+        const questions = await this.axios.get(`/question/list/${this.coordinates}`)
+        this.questions = questions.data
+      } catch (err) {
+        console.log(err.message)
+      }
+    }
+  }
+}
+</script>
