1416f192961b72ef6b403d34092550f49229b328
mehdi-alouane <pro4medo@gmail.com>
2019-07-02 11:07:59 +0100

    refactor: add questionCard to favourites

--- client/src/views/Favourites.vue
+++ client/src/views/Favourites.vue
@@ -1,31 +1,46 @@
 <template>
   <section class="section">
     <div class="container">
-      <div class="box">
-        <p>{{ favourites }}</p>
-      </div>
-
       <h1 class="title">
         Favourites questions:
       </h1>
+
+      <div class="columns is-multiline is-mobile">
+        <div
+          v-for="question in favourites"
+          :key="question._id"
+          class="column is-4"
+        >
+          <question-card
+            :show-remove-button="true"
+            :question="question"
+          />
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
+import questionCard from '@/components/QuestionCard'
+
 export default {
+  components: { questionCard },
   data: () => ({
     favourites: null
   }),
   created () {
     this.getFavouriteQuestions()
   },
+  watch: {
+    '$route': 'getFavouriteQuestions'
+  },
   methods: {
     async getFavouriteQuestions () {
       const userID = this.$store.state.user.id
-      const favourites = await this.axios.get(`/favourite/list/${userID}`)
-      this.favourites = favourites.data
-      console.log(this.favourites)
+      const { data } = await this.axios.get(`/favourite/list/${userID}`)
+      // console.log(data)
+      this.favourites = data.questions
     }
   }
 }
